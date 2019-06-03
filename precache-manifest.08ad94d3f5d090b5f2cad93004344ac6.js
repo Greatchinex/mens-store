45d1cb44f215939d18bbd481142718ec139cbd71
Chilaka Chinedu <greatchinex@gmail.com>
2019-06-03 17:23:19 +0100

    Updates

---
+++ precache-manifest.08ad94d3f5d090b5f2cad93004344ac6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7e7eed10b1e44f1c5dec2d182bbc5b",
+    "url": "/mens-store/index.html"
+  },
+  {
+    "revision": "fc1e8ae7489e3011684b",
+    "url": "/mens-store/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "b2db8440007847a96a2d",
+    "url": "/mens-store/static/css/main.331002a0.chunk.css"
+  },
+  {
+    "revision": "fc1e8ae7489e3011684b",
+    "url": "/mens-store/static/js/2.c0d29d8a.chunk.js"
+  },
+  {
+    "revision": "b2db8440007847a96a2d",
+    "url": "/mens-store/static/js/main.cce654cc.chunk.js"
+  },
+  {
+    "revision": "ce8206c6b07bfd837bc5",
+    "url": "/mens-store/static/js/runtime~main.52a495e7.js"
+  }
+]);
